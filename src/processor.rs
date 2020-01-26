143cea0ad71b904f28b5e1c54aa99b6eb398609e
Danielle Brook-Roberge <danielle@brook-roberge.ca>
2020-01-25 17:41:47 -0800

    Initial CPU with only registers

---
+++ src/processor.rs
@@ -0,0 +1,26 @@
+#[derive(Default, Clone)]
+pub struct Processor {
+    pub a: u8,
+    pub x: u8,
+    pub y: u8,
+}
+
+impl Processor {
+    pub fn new() -> Processor {
+        Processor::default()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_processor_new() {
+        let cpu = Processor::new();
+
+        assert_eq!(cpu.a, 0);
+        assert_eq!(cpu.x, 0);
+        assert_eq!(cpu.y, 0);
+    }
+}
