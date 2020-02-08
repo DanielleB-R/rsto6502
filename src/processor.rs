18e79a80672933d36113a88f3e102c5d59859cdc
Danielle Brook-Roberge <danielle@brook-roberge.ca>
2020-02-07 18:17:24 -0800

    Add register transfer instructions.

--- src/processor.rs
+++ src/processor.rs
@@ -73,12 +73,36 @@ impl Processor {
     pub(crate) fn sty(&mut self, addr: u16) {
         self.memory.write(addr, self.core.y);
     }
+
+    pub(crate) fn tax(&mut self, _addr: u16) {
+        self.core.x = self.core.a;
+        self.core.f.set_z(self.core.x);
+        self.core.f.set_n(self.core.x);
+    }
+
+    pub(crate) fn tay(&mut self, _addr: u16) {
+        self.core.y = self.core.a;
+        self.core.f.set_z(self.core.y);
+        self.core.f.set_n(self.core.y);
+    }
+
+    pub(crate) fn txa(&mut self, _addr: u16) {
+        self.core.a = self.core.x;
+        self.core.f.set_z(self.core.a);
+        self.core.f.set_n(self.core.a);
+    }
+
+    pub(crate) fn tya(&mut self, _addr: u16) {
+        self.core.a = self.core.y;
+        self.core.f.set_z(self.core.a);
+        self.core.f.set_n(self.core.a);
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::alter_default_by;
+    use crate::{alter_by, alter_default_by};
 
     #[test]
     fn test_core_new() {
@@ -189,5 +213,108 @@ mod tests {
 
         cpu.sty(addr);
         assert_eq!(val, cpu.memory.contents[addr as usize]);
+    }
+
+    #[test]
+    fn test_tax() {
+        let mut cpu = Processor::new();
+
+        let val = 0xff;
+        cpu.core.a = val;
+        let expected = alter_by!(cpu.core, x => val, f.n => true);
+
+        cpu.tax(0);
+        assert_eq!(expected, cpu.core);
+
+        let val = 0x00;
+        cpu.core.a = val;
+        let expected = alter_by!(cpu.core, x => val, f.n => false, f.z => true);
+
+        cpu.tax(0);
+        assert_eq!(expected, cpu.core);
+
+        let val = 0x54;
+        cpu.core.a = val;
+        let expected = alter_by!(cpu.core, x => val, f.z => false);
+
+        cpu.tax(0);
+        assert_eq!(expected, cpu.core);
+    }
+
+    #[test]
+    fn test_tay() {
+        let mut cpu = Processor::new();
+
+        let val = 0xff;
+        cpu.core.a = val;
+        let expected = alter_by!(cpu.core, y => val, f.n => true);
+
+        cpu.tay(0);
+        assert_eq!(expected, cpu.core);
+
+        let val = 0x00;
+        cpu.core.a = val;
+        let expected = alter_by!(cpu.core, y => val, f.n => false, f.z => true);
+
+        cpu.tay(0);
+        assert_eq!(expected, cpu.core);
+
+        let val = 0x54;
+        cpu.core.a = val;
+        let expected = alter_by!(cpu.core, y => val, f.z => false);
+
+        cpu.tay(0);
+        assert_eq!(expected, cpu.core);
+    }
+
+    #[test]
+    fn test_txa() {
+        let mut cpu = Processor::new();
+
+        let val = 0xff;
+        cpu.core.x = val;
+        let expected = alter_by!(cpu.core, a => val, f.n => true);
+
+        cpu.txa(0);
+        assert_eq!(expected, cpu.core);
+
+        let val = 0x00;
+        cpu.core.x = val;
+        let expected = alter_by!(cpu.core, a => val, f.n => false, f.z => true);
+
+        cpu.txa(0);
+        assert_eq!(expected, cpu.core);
+
+        let val = 0x54;
+        cpu.core.x = val;
+        let expected = alter_by!(cpu.core, a => val, f.z => false);
+
+        cpu.txa(0);
+        assert_eq!(expected, cpu.core);
+    }
+    #[test]
+    fn test_tya() {
+        let mut cpu = Processor::new();
+
+        let val = 0xff;
+        cpu.core.y = val;
+        let expected = alter_by!(cpu.core, a => val, f.n => true);
+
+        cpu.tya(0);
+        assert_eq!(expected, cpu.core);
+
+        let val = 0x00;
+        cpu.core.y = val;
+        let expected = alter_by!(cpu.core, a => val, f.n => false, f.z => true);
+
+        cpu.tya(0);
+        assert_eq!(expected, cpu.core);
+
+        let val = 0x54;
+        cpu.core.y = val;
+        let expected = alter_by!(cpu.core, a => val, f.z => false);
+
+        cpu.tya(0);
+        assert_eq!(expected, cpu.core);
     }
 }
