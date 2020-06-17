7d8ef5532e476faa7e59fe4c1da8f6f6ee07584a
Danielle Brook-Roberge <danielle@brook-roberge.ca>
2020-06-16 20:56:59 -0700

    Add more memory types

--- src/memory.rs
+++ src/memory.rs
@@ -36,3 +36,66 @@ impl Memory for RandomAccessMemory {
         self.contents.len()
     }
 }
+
+#[derive(Debug, Clone)]
+pub struct ReadOnlyMemory {
+    pub contents: Vec<u8>,
+}
+
+impl ReadOnlyMemory {
+    pub fn new(size: u16) -> Self {
+        Self {
+            contents: vec![0; size as usize],
+        }
+    }
+}
+
+impl Memory for ReadOnlyMemory {
+    fn read(&self, addr: u16) -> u8 {
+        self.contents[addr as usize]
+    }
+
+    fn write(&mut self, _addr: u16, _data: u8) {
+        // Silently discard the write
+    }
+
+    fn length(&self) -> usize {
+        self.contents.len()
+    }
+}
+
+pub struct MirroredMemory<T: Memory> {
+    pub underlying: T,
+    pub mask: u16,
+    pub size: usize,
+}
+
+impl<T> MirroredMemory<T>
+where
+    T: Memory,
+{
+    pub fn new(underlying: T, mask: u16, size: usize) -> Self {
+        Self {
+            underlying,
+            mask,
+            size,
+        }
+    }
+}
+
+impl<T> Memory for MirroredMemory<T>
+where
+    T: Memory,
+{
+    fn read(&self, addr: u16) -> u8 {
+        self.underlying.read(addr & self.mask)
+    }
+
+    fn write(&mut self, addr: u16, data: u8) {
+        self.underlying.write(addr & self.mask, data)
+    }
+
+    fn length(&self) -> usize {
+        self.size
+    }
+}
